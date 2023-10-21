71a5a2f951092dd48e9f0136fc6824a47e53de26
Lokesh396 <lokeshkumarreddy.c@gmail.com>
2023-10-21 16:08:22 +0530

    about component was updated

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,54 @@
-import reactImg from '../../assets/react.svg'
+import PythonIcon from "../../assets/images/python-icon.svg";
+import FullStackImg from "../../assets/images/full-stack-24.png";
 
 function About() {
   return (
-    <div style={{backgroundColor:"white"}} className='p-3 mt-3 rounded'>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, aliquid libero autem temporibus et aspernatur molestiae beatae fugit nesciunt ipsam maiores quibusdam laborum harum, omnis adipisci repudiandae, quaerat provident at!</p>
-      <img src={reactImg} alt="home" />
+    <div className=" about-page mt-3 rounded">
+      <h1>ABOUT ME</h1>
+
+      <p className="my-3 fw-medium">
+        Hello there! I am thrilled to welcome you to my portfolio. I am a
+        passionate and versatile full-stack developer with a keen interest in
+        exploring the latest cutting-edge technologies. My journey in the world
+        of web development has been nothing short of exhilarating, and I
+        constantly strive to enhance my skills and embrace emerging trends in
+        the industry.
+      </p>
+      <h1>What I do!</h1>
+      <div className="d-flex gap-3 justify-content-between mt-3">
+        <div className="about-card">
+          <h3>
+            <img
+              style={{ width: "24px", height: "24px" }}
+              src={FullStackImg}
+            ></img>{" "}
+            Web Development
+          </h3>
+          <p>
+            As a developer, I find myself most captivated by the power and
+            flexibility of NEXT.js. I am always eager to dive into new projects
+            that leverage NEXT.js and discover innovative ways to create fast,
+            scalable, and user-friendly applications.
+          </p>
+        </div>
+        <div className="about-card">
+          <h3>
+            <img
+              style={{ width: "24px", height: "24px" }}
+              src={PythonIcon}
+            ></img>{" "}
+            Python Programming
+          </h3>
+          <p>
+            As a developer, I find myself most captivated by the power and
+            flexibility of NEXT.js. I am always eager to dive into new projects
+            that leverage NEXT.js and discover innovative ways to create fast,
+            scalable, and user-friendly applications.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/pages/ContactCard.jsx
+++ src/components/pages/ContactCard.jsx
@@ -1,41 +1,41 @@
-import * as Lucide from 'lucide-react'
-import GmailLogo from '../../assets/images/gmail-logo.png'
-import LocationImg from "../../assets/images/location.png"
-import SmartPhoneImg from "../../assets/images/smartphone.png"
+import * as Lucide from "lucide-react";
+import GmailLogo from "../../assets/images/gmail-logo.png";
+import LocationImg from "../../assets/images/location.png";
+import SmartPhoneImg from "../../assets/images/smartphone.png";
 function ContactCard() {
   return (
-    <div className="contact-card mt-3 pb-2 px-2">
-        <div className='d-flex  align-items-center  gap-3  py-3 contact-icon'>  
-            <img src={SmartPhoneImg} alt="SmartPhone" />
-            <div>
-                <p  className='text-color'>Phone</p>
-                <p>+919502921597</p>
-            </div>
+    <div className="contact-card mt-3 pb-2 ">
+      <div className="d-flex  align-items-center  gap-3  py-3 contact-icon">
+        <img src={SmartPhoneImg} alt="SmartPhone" />
+        <div>
+          <p className="text-color">Phone</p>
+          <p>+919502921597</p>
         </div>
-        <div className='d-flex  align-items-center  gap-3  py-3 contact-icon'>
+      </div>
+      <div className="d-flex  align-items-center  gap-3  py-3 contact-icon">
         <img src={GmailLogo} alt="Gmail" />
-            <div>
-                <p className='text-color'>Email</p>
-                <p>lokeshkumarreddy.c@gmail.com</p>
-            </div>
+        <div>
+          <p className="text-color">Email</p>
+          <p>lokeshkumarreddy.c@gmail.com</p>
         </div>
-        <div className='d-flex  align-items-center  gap-3  py-3 contact-icon'>
+      </div>
+      <div className="d-flex  align-items-center  gap-3  py-3 contact-icon">
         <img src={LocationImg} alt="Location" />
-            <div >
-                <p className='text-color'>Location</p>
-                <p>Hyderabad, Telangana</p>
-            </div>
+        <div>
+          <p className="text-color">Location</p>
+          <p>Hyderabad, Telangana</p>
         </div>
-        <div className='py-3 d-flex justify-content-center'>
-            <button className='btn btn-bg'>
-                <div className='d-flex justify-content align-items gap-2'>
-                <Lucide.DownloadIcon></Lucide.DownloadIcon>
-                Download Resume
-                </div>
-            </button>
-        </div>
+      </div>
+      <div className="py-3 d-flex justify-content-center">
+        <button className="btn btn-bg">
+          <div className="d-flex justify-content align-items gap-2">
+            <Lucide.DownloadIcon></Lucide.DownloadIcon>
+            Download Resume
+          </div>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactCard
+export default ContactCard;
